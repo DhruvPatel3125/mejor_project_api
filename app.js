98b71a59519e4ce62ceea2d2932701ea4ecda20b
dhruv <dhruvjpatel5@gmail.com>
2024-10-12 11:30:48 +0530

    "Added new routes for creating and showing listings, updated listing model and index view"

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ async function main(){
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 
 app.get("/",(req,res)=>{
@@ -30,11 +31,30 @@ app.get("/",(req,res)=>{
 
 
 //Index route
-app.get("/listing", async(req,res) =>{
+app.get("/listings", async(req,res) =>{
   const allListing = await Listing.find({})
   res.render("listings/index.ejs",{allListing});
 });
 
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+})
+
+//show route
+app.get("/listings/:id", async(req, res)=>{
+    let {id} = req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/show.ejs",{listing});
+})
+
+//create route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  });
+  
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing = new Listing({
 //         title:"my new villa",
