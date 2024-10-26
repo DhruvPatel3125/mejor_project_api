5b58aa960539a6bec94be468a14270a7d2656e93
dhruv <dhruvjpatel5@gmail.com>
2024-10-26 10:16:39 +0530

    "Updated app.js with formatting changes, added error handling, and modified routes. Removed script.js file. Updated edit.ejs and new.ejs with Bootstrap validation and added required attributes to form fields."

--- app.js
+++ app.js
@@ -6,82 +6,86 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-const mongo_url = "mongodb://127.0.0.1:27017/dhruv";  
+const mongo_url = "mongodb://127.0.0.1:27017/dhruv";
 
 main()
-    .then(()=>{
-        console.log("connected to db")
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-
-async function main(){
-    await mongoose.connect(mongo_url)
-}
-
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate)
-app.use(express.static(path.join(__dirname,"/public")));
-
-
-app.get("/", (req, res) => {
-    res.send("Hi, I am root");
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-//Index route
-app.get("/listings", async(req,res) =>{
-  const allListing = await Listing.find({})
-  res.render("listings/index.ejs",{allListing});
+async function main() {
+  await mongoose.connect(mongo_url);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
+//Index route
+app.get("/listings", async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("listings/index.ejs", { allListing });
+});
 
 //new route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs")
-})
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
 
 //show route
-app.get("/listings/:id", async(req, res)=>{
-    let {id} = req.params;
-   const listing = await Listing.findById(id);
-   res.render("listings/show.ejs",{listing});
-})
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
 
 //create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", async (req, res, next) => {
+  try {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-  });
-  
- //edit route
- app.get("/listings/:id/edit",async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
- })
+  } catch (err) {
+    next(err);
+  }
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
 
 //Update Route
 app.put("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-  });
-
-  //delete route
-  app.delete("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-  })
-
-app.listen (3000,()=>{
-    console.log("server is running on port 3000");
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
- 
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+app.use((err, req, res, next) => {
+  res.send("somthing where wrong");
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
