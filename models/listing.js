91fb2e39ce2a017d0b44b2cc782c118cce4d63e1
dhruv <dhruvjpatel5@gmail.com>
2024-11-14 11:16:45 +0530

    "Refactor Listing model schema: simplify image field, remove filename, and update price field; add review model reference"

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -8,17 +9,15 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    filename: String, // Stores the filename of the image
-    // Direct URL as a string without the object structure
-    url: {
-      type: String,
-      default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60", // Default image URL directly
-    },
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
   },
-  price: {
-    type: Number,
-    required: true,
-  },
+  price: Number,
   location: String,
   country: String,
   reviews: [
@@ -29,6 +28,5 @@ const listingSchema = new Schema({
   ],
 });
 
-// Create and export the Listing model based on the schema
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
